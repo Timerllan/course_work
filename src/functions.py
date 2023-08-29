16ea0e0265738d3f1297eb03b1dd6bbe33f47ef5
Shepetkov Timerlan <sergeevp284@gmail.com>
2023-08-29 13:36:31 +0300

    func executed operations

--- src/functions.py
+++ src/functions.py
@@ -1,7 +1,18 @@
 import json
 
+FILE_BANK = 'operations.json'
 
-with open('operations.json','r',encoding='utf-8')as file:
-    bank_file = json.load(file)
+def func_load_file(file_name):
+    with open('operations.json','r',encoding='utf-8')as file:
+        bank_file = json.load(file)
+    return bank_file
 
-print(bank_file)
+
+def executed_operations(operation_list):
+    return [operation for operation in operation_list if operation and operation["state"]== "EXECUTED"]
+
+
+
+
+
+
