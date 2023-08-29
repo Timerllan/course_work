7ba51d4b06668fb066cf4e42ef2d91b9ee61662d
Shepetkov Timerlan <sergeevp284@gmail.com>
2023-08-29 13:52:06 +0300

    func lambda operations list

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+
 
 FILE_BANK = 'operations.json'
 
@@ -10,6 +12,10 @@ def func_load_file(file_name):
 
 def executed_operations(operation_list):
     return [operation for operation in operation_list if operation and operation["state"]== "EXECUTED"]
+
+
+def sorted_operations(operation_list):
+    return sorted(operation_list,key=lambda x:datetime.datetime.strptime(x['date'],'%Y-%m-%dT%H:%M:%S.%f'),reverse=True)
 
 
 
